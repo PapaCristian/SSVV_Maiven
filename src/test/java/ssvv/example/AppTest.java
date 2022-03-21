cc026f8e00bdcbc64325a4875e91ee433e5955a2
Hiumee <cristianpapa2000@yahoo.com>
2022-03-21 18:25:08 +0200

    Fixing tests

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,14 +1,17 @@
 package ssvv.example;
 
+import domain.Nota;
 import domain.Student;
+import domain.Tema;
 import org.junit.Test;
-import repository.StudentFileRepository;
-import validation.StudentValidator;
-import validation.ValidationException;
-import validation.Validator;
+import repository.*;
+import service.Service;
+import validation.*;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
@@ -29,7 +32,9 @@ public class AppTest
     private void DeleteFileContents(String file) {
         try {
             PrintWriter writer = new PrintWriter(file);
-            writer.print("");
+            writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                    "<Entitati>\n" +
+                    "</Entitati>\n");
             writer.close();
         } catch (Exception e) {}
     }
@@ -39,11 +44,42 @@ public class AppTest
         return openFile.length() == 0;
     }
 
-    private StudentFileRepository GetEmptyStudentsRepository() {
+    private boolean CheckIfStudentPresent(Iterable<Student> iterator, Student student) {
+        for (Student value : iterator) {
+            if (value.getID().equals(student.getID()) && value.getNume().equals(student.getNume()) && value.getGrupa() == student.getGrupa()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Service GetService() {
+        StudentXMLRepository fileRepository1 = GetEmptyStudentsRepository();
+        TemaXMLRepository fileRepository2 = GetEmptyHWRepository();
+        NotaXMLRepository fileRepository3 = GetEmptyGradeRepository();
+
+        return new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    private StudentXMLRepository GetEmptyStudentsRepository() {
         Validator<Student> studentValidator = new StudentValidator();
-        String fileName = "studenti_test.txt";
+        String fileName = "studenti_test.xml";
         DeleteFileContents(fileName);
-        return new StudentFileRepository(studentValidator, fileName);
+        return new StudentXMLRepository(studentValidator, fileName);
+    }
+
+    private TemaXMLRepository GetEmptyHWRepository() {
+        Validator<Tema> validator = new TemaValidator();
+        String fileName = "hw_test.xml";
+        DeleteFileContents(fileName);
+        return new TemaXMLRepository(validator, fileName);
+    }
+
+    private NotaXMLRepository GetEmptyGradeRepository() {
+        Validator<Nota> validator = new NotaValidator();
+        String fileName = "grade_test.xml";
+        DeleteFileContents(fileName);
+        return new NotaXMLRepository(validator, fileName);
     }
 
     @Test
@@ -74,114 +110,51 @@ public class AppTest
 
     @Test
     public void test_tc_1() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("", "A", 225);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) { }
-
-        try {
-            repository.findOne("11");
-            fail();
-        } catch (Exception ignored) { }
+        assertEquals(service.saveStudent("", "A", 225), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("", "A", 225)));
     }
 
     @Test
     public void test_tc_2() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "", 225);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) { }
-
-        try {
-            repository.findOne("11");
-            fail();
-        } catch (Exception ignored) { }
+        assertEquals(service.saveStudent("11", "", 225), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "", 225)));
     }
 
     @Test
     public void test_tc_3() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 1);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) { }
-
-        try {
-            repository.findOne("11");
-            fail();
-        } catch (Exception ignored) { }
+        assertEquals(service.saveStudent("11", "A", 1), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 1)));
     }
 
     @Test
     public void test_tc_4() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 225);
-
-        try {
-            repository.save(student);
-        } catch (ValidationException ignored) {
-            fail();
-        }
-
-        try {
-            repository.findOne("11");
-        } catch (Exception e) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "A", 225), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 225)));
     }
 
     @Test
     public void test_tc_5() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
+        service.saveStudent("11", "A", 226);
 
-        Student student = new Student("11", "A", 225);
-        repository.save(student);
-        student = new Student("11", "B", 226);
-
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        if (repository.findOne("11").getNume().equals("B")) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "B", 226), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "B", 226)));
     }
 
     @Test
     public void test_tc_6() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 225);
-        repository.save(student);
-        student = new Student("12", "B", 226);
-
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("12");
-        } catch (Exception e) {
-            fail();
-        }
+        assertEquals(service.saveStudent("12", "B", 226), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("12", "B", 226)));
     }
 
     @Test
@@ -191,113 +164,49 @@ public class AppTest
 
     @Test
     public void test_tc_8() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 110);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-            fail();
-        } catch (Exception ignored) { }
+        assertEquals(service.saveStudent("11", "A", 110), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 110)));
     }
 
     @Test
     public void test_tc_9() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 111);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-        } catch (Exception ignored) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "A", 111), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 111)));
     }
 
     @Test
     public void test_tc_10() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 112);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-        } catch (Exception ignored) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "A", 112), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 112)));
     }
 
     @Test
     public void test_tc_11() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 936);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-        } catch (Exception ignored) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "A", 936), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 936)));
     }
 
     @Test
     public void test_tc_12() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 937);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-        } catch (Exception ignored) {
-            fail();
-        }
+        assertEquals(service.saveStudent("11", "A", 937), 0);
+        assertTrue(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 937)));
     }
 
     @Test
     public void test_tc_13() {
-        StudentFileRepository repository = GetEmptyStudentsRepository();
+        Service service = GetService();
 
-        Student student = new Student("11", "A", 938);
-
-        try {
-            repository.save(student);
-            fail();
-        } catch (ValidationException ignored) {
-        }
-
-        try {
-            repository.findOne("11");
-            fail();
-        } catch (Exception ignored) { }
+        assertEquals(service.saveStudent("11", "A", 938), 1);
+        assertFalse(CheckIfStudentPresent(service.findAllStudents(), new Student("11", "A", 938)));
     }
 }
