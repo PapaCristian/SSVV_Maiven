63889bd724fdd919493bd2e060020f3e8a3e801f
Hiumee <cristianpapa2000@yahoo.com>
2022-03-21 17:20:44 +0200

    Adding more tests

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -3,8 +3,8 @@ package ssvv.example;
 import domain.Student;
 import org.junit.Test;
 import repository.StudentFileRepository;
-import repository.StudentXMLRepository;
 import validation.StudentValidator;
+import validation.ValidationException;
 import validation.Validator;
 
 import java.io.File;
@@ -39,6 +39,13 @@ public class AppTest
         return openFile.length() == 0;
     }
 
+    private StudentFileRepository GetEmptyStudentsRepository() {
+        Validator<Student> studentValidator = new StudentValidator();
+        String fileName = "studenti_test.txt";
+        DeleteFileContents(fileName);
+        return new StudentFileRepository(studentValidator, fileName);
+    }
+
     @Test
     public void shouldWriteToFileValidStudent_StudentFileRepository() {
         String fileName = "studenti_test.txt";
@@ -63,5 +70,234 @@ public class AppTest
         fileRepository.save(validStudent);
 
         assertTrue(isFileEmpty(fileName));
+    }
+
+    @Test
+    public void test_tc_1() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("", "A", 225);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) { }
+
+        try {
+            repository.findOne("11");
+            fail();
+        } catch (Exception ignored) { }
+    }
+
+    @Test
+    public void test_tc_2() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "", 225);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) { }
+
+        try {
+            repository.findOne("11");
+            fail();
+        } catch (Exception ignored) { }
+    }
+
+    @Test
+    public void test_tc_3() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 1);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) { }
+
+        try {
+            repository.findOne("11");
+            fail();
+        } catch (Exception ignored) { }
+    }
+
+    @Test
+    public void test_tc_4() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 225);
+
+        try {
+            repository.save(student);
+        } catch (ValidationException ignored) {
+            fail();
+        }
+
+        try {
+            repository.findOne("11");
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_5() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 225);
+        repository.save(student);
+        student = new Student("11", "B", 226);
+
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        if (repository.findOne("11").getNume().equals("B")) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_6() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 225);
+        repository.save(student);
+        student = new Student("12", "B", 226);
+
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("12");
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_7() {
+
+    }
+
+    @Test
+    public void test_tc_8() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 110);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+            fail();
+        } catch (Exception ignored) { }
+    }
+
+    @Test
+    public void test_tc_9() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 111);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+        } catch (Exception ignored) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_10() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 112);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+        } catch (Exception ignored) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_11() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 936);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+        } catch (Exception ignored) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_12() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 937);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+        } catch (Exception ignored) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_tc_13() {
+        StudentFileRepository repository = GetEmptyStudentsRepository();
+
+        Student student = new Student("11", "A", 938);
+
+        try {
+            repository.save(student);
+            fail();
+        } catch (ValidationException ignored) {
+        }
+
+        try {
+            repository.findOne("11");
+            fail();
+        } catch (Exception ignored) { }
     }
 }
